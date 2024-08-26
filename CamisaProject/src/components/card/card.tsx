54ae0e2bbc6a97fe9cc49807d6369cb80eae7a70
gabriel.kaminise <gabriel.kaminise@sankhya.com.br>
2024-08-26 11:35:35 -0300

    Commit com pequenos ajustes visuais

--- CamisaProject/src/components/card/card.tsx
+++ CamisaProject/src/components/card/card.tsx
@@ -9,7 +9,7 @@ interface Props {
 export function Card({price, image, title} : Props) {
   return(
       <div className="card col-6" style={{width: "20rem"}} >
-        <img src={image} className="card-img-top" style={{ maxWidth: "17rem", minWidth: "17rem", maxHeight: "20rem", minHeight: "20rem" }} />
+        <img src={image} className="card-img-top" style={{ maxWidth: "17rem", minWidth: "17rem", maxHeight: "20rem", minHeight: "20rem", padding: "5px 0px 5px 20px" }} />
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
           <p className="card-text">Preco: {price},00</p>
